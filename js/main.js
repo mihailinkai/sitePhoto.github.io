8954d0d4e32d6064b505381ccd042af72fa071cd
mihailinkai <misha.izgareva@gmail.com>
2019-05-21 23:35:23 +0300

    resume site for photographer

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+jQuery(document).ready(function($) {
+
+    // Добавляем маску для поля с номера телефона
+    $('#phone').mask('+7 (999) 999-99-99');
+
+    // Проверяет отмечен ли чекбокс согласия
+    // с обработкой персональных данных
+    $('#check').on('click', function() {
+        if ($("#check").prop("checked")) {
+            $('#button').attr('disabled', false);
+        } else {
+            $('#button').attr('disabled', true);
+        }
+    });
+
+    // Отправляет данные из формы на сервер и получает ответ
+    $('#contactForm').on('submit', function(event) {
+        
+        event.preventDefault();
+
+        var form = $('#contactForm'),
+            button = $('#button'),
+            answer = $('#answer'),
+            loader = $('#loader');
+
+        $.ajax({
+            url: 'handler.php',
+            type: 'POST',
+            data: form.serialize(),
+            beforeSend: function() {
+                answer.empty();
+                button.attr('disabled', true).css('margin-bottom', '20px');
+                loader.fadeIn();
+            },
+            success: function(result) {
+                loader.fadeOut(300, function() {
+                    answer.text(result);
+                });
+                form.find('.form-control').val('');
+                button.attr('disabled', false);
+				 $('#exampleModal').modal('hide');
+            },
+            error: function() {
+                loader.fadeOut(300, function() {
+                    answer.text('Произошла ошибка! Попробуйте позже.');
+                });
+                button.attr('disabled', false);
+            }
+        });
+    
+    });
+
+});
+
+
